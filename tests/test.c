bc884465ff568e6357dbdb398ac87d0a5935bcf8
周正辉 <zhouzhenghui@ticpsh.com>
2019-12-05 15:03:07 +0800

    Modify tests for CI

--- tests/test.c
+++ tests/test.c
@@ -8,4 +8,8 @@ struct foo {
 STATIC_ASSERT(offsetof(struct foo, string) == 0, string_should_be_first_member_of_struct_foo);
 typedef STATIC_ASSERT_OR_TYPE(offsetof(struct foo, string) == 0, string_should_be_first_member_of_struct_foo) foo_type;
 STATIC_ASSERT(STATIC_ASSERT_OR_ZERO(offsetof(struct foo, string) == 0, string_should_be_first_member_of_struct_foo) == 0, STATIC_ASSERT_OR_ZERO_should_equal_zero);
-STATIC_ASSERT(0, THIS_ERROR_MESSAGE_TELLS_THAT_ALL_TESTS_ARE_SUCCESSFUL);
+
+int main()
+{
+  return 0;
+}
